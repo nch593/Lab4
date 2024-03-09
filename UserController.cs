8d0a7e6eb601ad1066786ff76d30868dcf426245
nch593 <158087957+nch593@users.noreply.github.com>
2024-03-08 23:18:12 -0500

    Lab #4

---
+++ UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        UsersContext uc = new();
+        public IActionResult YKView()
+        {
+            return View(uc.YkumarTables.ToList());
+        }
+    }
+}
